b5eb7434400306e609d97dcbfb2e0aaf44a9b665
Brian Ail <ail.brianez@gmail.com>
2019-08-12 15:56:30 -0300

    Login de cliente de mail pop3

--- mailClient/mailClient.c
+++ mailClient/mailClient.c
@@ -4,6 +4,8 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include "../libs/tcpClient.h"
+#include <string.h>
+
 
 int main(int argc, char const *argv[])
 {
@@ -20,4 +22,33 @@ int main(int argc, char const *argv[])
 	printf("Received: %s\n", buffer);
 	close(sock);
 	return 0;
+}
+
+int login(char * user, char * passwd, int sock) {
+	char * userMsg;
+	char * passwdMsg;
+	generateLoginStrings(userMsg,passwdMsg,user,passwd);
+	
+	ssize_t numBytes = send(sock, userMsg, userLen, 0);
+	if (!successfulUser(numBytes,sock)) {
+		perror("Invalid username");
+		exit(1);
+	}
+
+	ssize_t numBytes = send(sock, user, userLen, 0);
+
+}
+void generateLoginStrings(char * userMsg, char *passwdMsg, user, passwd) {
+	// Generate user and password strings
+	char *userBase = "user ";
+	int userLen = strlen(userBase) + strlen(user);
+	*userMsg = malloc(1+userLen);
+	strcpy(userMsg, userBase);
+	strcat(userMsg, user);
+
+	char *passwdBase = "pass ";
+	int passwdLen = strlen(passwdBase) + strlen(passwd);
+	*passwdMsg = malloc(1+passwdLen);
+	strcpy(passwdMsg, passwdBase);
+	strcat(passwdMsg, passwd);
 }
